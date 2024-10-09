e7b483dbff58a9e3d343b94f08d759e5399250e0
Quang-Dieu Nguyen <86088186+itzmealvin@users.noreply.github.com>
2024-10-09 12:00:00 +0700

    gitfiti

---
+++ gitfiti.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+REPO=gitfiti
+git init $REPO
+cd $REPO
+touch README.md
+git add README.md
+touch gitfiti
+git add gitfiti
+GIT_AUTHOR_DATE=2024-08-26T12:00:00 GIT_COMMITTER_DATE=2024-08-26T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-08-26T12:00:00 GIT_COMMITTER_DATE=2024-08-26T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-08-26T12:00:00 GIT_COMMITTER_DATE=2024-08-26T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-08-26T12:00:00 GIT_COMMITTER_DATE=2024-08-26T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-08-27T12:00:00 GIT_COMMITTER_DATE=2024-08-27T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-08-27T12:00:00 GIT_COMMITTER_DATE=2024-08-27T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-08-27T12:00:00 GIT_COMMITTER_DATE=2024-08-27T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-08-27T12:00:00 GIT_COMMITTER_DATE=2024-08-27T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-08-28T12:00:00 GIT_COMMITTER_DATE=2024-08-28T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-08-28T12:00:00 GIT_COMMITTER_DATE=2024-08-28T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-08-28T12:00:00 GIT_COMMITTER_DATE=2024-08-28T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-08-28T12:00:00 GIT_COMMITTER_DATE=2024-08-28T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-01T12:00:00 GIT_COMMITTER_DATE=2024-09-01T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-01T12:00:00 GIT_COMMITTER_DATE=2024-09-01T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-01T12:00:00 GIT_COMMITTER_DATE=2024-09-01T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-01T12:00:00 GIT_COMMITTER_DATE=2024-09-01T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-02T12:00:00 GIT_COMMITTER_DATE=2024-09-02T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-02T12:00:00 GIT_COMMITTER_DATE=2024-09-02T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-04T12:00:00 GIT_COMMITTER_DATE=2024-09-04T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-04T12:00:00 GIT_COMMITTER_DATE=2024-09-04T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-05T12:00:00 GIT_COMMITTER_DATE=2024-09-05T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-05T12:00:00 GIT_COMMITTER_DATE=2024-09-05T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-05T12:00:00 GIT_COMMITTER_DATE=2024-09-05T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-05T12:00:00 GIT_COMMITTER_DATE=2024-09-05T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-08T12:00:00 GIT_COMMITTER_DATE=2024-09-08T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-08T12:00:00 GIT_COMMITTER_DATE=2024-09-08T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-08T12:00:00 GIT_COMMITTER_DATE=2024-09-08T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-08T12:00:00 GIT_COMMITTER_DATE=2024-09-08T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-10T12:00:00 GIT_COMMITTER_DATE=2024-09-10T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-10T12:00:00 GIT_COMMITTER_DATE=2024-09-10T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-11T12:00:00 GIT_COMMITTER_DATE=2024-09-11T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-11T12:00:00 GIT_COMMITTER_DATE=2024-09-11T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-12T12:00:00 GIT_COMMITTER_DATE=2024-09-12T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-12T12:00:00 GIT_COMMITTER_DATE=2024-09-12T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-13T12:00:00 GIT_COMMITTER_DATE=2024-09-13T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-13T12:00:00 GIT_COMMITTER_DATE=2024-09-13T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-13T12:00:00 GIT_COMMITTER_DATE=2024-09-13T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-13T12:00:00 GIT_COMMITTER_DATE=2024-09-13T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-16T12:00:00 GIT_COMMITTER_DATE=2024-09-16T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-16T12:00:00 GIT_COMMITTER_DATE=2024-09-16T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-16T12:00:00 GIT_COMMITTER_DATE=2024-09-16T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-16T12:00:00 GIT_COMMITTER_DATE=2024-09-16T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-17T12:00:00 GIT_COMMITTER_DATE=2024-09-17T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-17T12:00:00 GIT_COMMITTER_DATE=2024-09-17T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-18T12:00:00 GIT_COMMITTER_DATE=2024-09-18T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-18T12:00:00 GIT_COMMITTER_DATE=2024-09-18T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-19T12:00:00 GIT_COMMITTER_DATE=2024-09-19T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-19T12:00:00 GIT_COMMITTER_DATE=2024-09-19T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-20T12:00:00 GIT_COMMITTER_DATE=2024-09-20T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-20T12:00:00 GIT_COMMITTER_DATE=2024-09-20T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-21T12:00:00 GIT_COMMITTER_DATE=2024-09-21T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-21T12:00:00 GIT_COMMITTER_DATE=2024-09-21T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-21T12:00:00 GIT_COMMITTER_DATE=2024-09-21T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-21T12:00:00 GIT_COMMITTER_DATE=2024-09-21T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-22T12:00:00 GIT_COMMITTER_DATE=2024-09-22T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-22T12:00:00 GIT_COMMITTER_DATE=2024-09-22T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-22T12:00:00 GIT_COMMITTER_DATE=2024-09-22T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-22T12:00:00 GIT_COMMITTER_DATE=2024-09-22T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-23T12:00:00 GIT_COMMITTER_DATE=2024-09-23T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-23T12:00:00 GIT_COMMITTER_DATE=2024-09-23T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-24T12:00:00 GIT_COMMITTER_DATE=2024-09-24T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-24T12:00:00 GIT_COMMITTER_DATE=2024-09-24T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-25T12:00:00 GIT_COMMITTER_DATE=2024-09-25T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-25T12:00:00 GIT_COMMITTER_DATE=2024-09-25T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-26T12:00:00 GIT_COMMITTER_DATE=2024-09-26T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-26T12:00:00 GIT_COMMITTER_DATE=2024-09-26T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-27T12:00:00 GIT_COMMITTER_DATE=2024-09-27T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-27T12:00:00 GIT_COMMITTER_DATE=2024-09-27T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-27T12:00:00 GIT_COMMITTER_DATE=2024-09-27T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-27T12:00:00 GIT_COMMITTER_DATE=2024-09-27T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-29T12:00:00 GIT_COMMITTER_DATE=2024-09-29T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-29T12:00:00 GIT_COMMITTER_DATE=2024-09-29T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-29T12:00:00 GIT_COMMITTER_DATE=2024-09-29T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-29T12:00:00 GIT_COMMITTER_DATE=2024-09-29T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-30T12:00:00 GIT_COMMITTER_DATE=2024-09-30T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-09-30T12:00:00 GIT_COMMITTER_DATE=2024-09-30T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-01T12:00:00 GIT_COMMITTER_DATE=2024-10-01T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-01T12:00:00 GIT_COMMITTER_DATE=2024-10-01T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-02T12:00:00 GIT_COMMITTER_DATE=2024-10-02T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-02T12:00:00 GIT_COMMITTER_DATE=2024-10-02T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-03T12:00:00 GIT_COMMITTER_DATE=2024-10-03T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-03T12:00:00 GIT_COMMITTER_DATE=2024-10-03T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-03T12:00:00 GIT_COMMITTER_DATE=2024-10-03T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-03T12:00:00 GIT_COMMITTER_DATE=2024-10-03T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-07T12:00:00 GIT_COMMITTER_DATE=2024-10-07T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-07T12:00:00 GIT_COMMITTER_DATE=2024-10-07T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-07T12:00:00 GIT_COMMITTER_DATE=2024-10-07T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-07T12:00:00 GIT_COMMITTER_DATE=2024-10-07T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-08T12:00:00 GIT_COMMITTER_DATE=2024-10-08T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-08T12:00:00 GIT_COMMITTER_DATE=2024-10-08T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-08T12:00:00 GIT_COMMITTER_DATE=2024-10-08T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-08T12:00:00 GIT_COMMITTER_DATE=2024-10-08T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-09T12:00:00 GIT_COMMITTER_DATE=2024-10-09T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-09T12:00:00 GIT_COMMITTER_DATE=2024-10-09T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-09T12:00:00 GIT_COMMITTER_DATE=2024-10-09T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+GIT_AUTHOR_DATE=2024-10-09T12:00:00 GIT_COMMITTER_DATE=2024-10-09T12:00:00 git commit --allow-empty -m "gitfiti" > /dev/null
+
+git branch -M main
+git remote add origin git@github.com:itzmealvin/$REPO.git
+git pull origin main
+git push -u origin main
